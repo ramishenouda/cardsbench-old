9cee59b129872d37c0e157abbeeecf9662174772
ramishenouda <ramishenoouda@outlook.com>
2020-08-23 19:30:30 +0200

    removed unnecessary files

--- CardsBench.API/Dtos/ListsForBoardDto.cs
+++
@@ -1,9 +0,0 @@
-namespace CardsBench.API.Dtos
-{
-    public class ListsForBoardDto
-    {
-        public string ListId { get; set; }
-        public string Title { get; set; }
-        public int Order { get; set; }
-    }
-}
